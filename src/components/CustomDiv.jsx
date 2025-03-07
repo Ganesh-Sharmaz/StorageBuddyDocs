57f205865b7646b5c9ccb3b112157828a74fe3b5
Ganesh-Sharmaz <shashanklhr@gmail.com>
2025-03-08 01:50:39 +0530

    custom ui div

---
+++ src/components/CustomDiv.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CustomDiv() {
+    return (
+        <div className=" border-b-1 h-10  border-gray-800 px-10 flex ">
+            <div className=" grow border-x-[1px] border-gray-800 flex ">
+                <div className=" pl-2 py-10 border-gray-800 flex flex-col text-sm"></div>
+            </div>
+        </div>
+    );
+}
+
+export default CustomDiv;
