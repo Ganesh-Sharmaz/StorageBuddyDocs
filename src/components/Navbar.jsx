d174b8a2765643c73da21d7615dc9ab7257deab2
Ganesh-Sharmaz <shashanklhr@gmail.com>
2025-03-09 22:27:54 +0530

    Navabar UI completed,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import githubsvg from "../assets/github-142-svgrepo-com.png";
 
 function Navbar() {
     return (
-        <div className="flex w-full items-center fixed justify-between text-white font-poppins h-14 backdrop-blur-[2px] px-6 border-b-[1px] border-[#1a1a1a]">
+        <div className=" z-50 flex w-full items-center fixed justify-between text-white font-poppins h-14 backdrop-blur-[2px] px-6 border-b-[1px] border-[#1a1a1a]">
             <div className="text-xl tracking-tighter">storagebuddy</div>
             <div className="text-sm flex space-x-4">
                 <button className="cursor-pointer">Docs</button>
